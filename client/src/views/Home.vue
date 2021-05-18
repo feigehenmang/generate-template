61649e3eae572a4192dca882f5cff43c0dcc7ef9
lijinfei <lijinfei@devcloud.com>
2021-05-18 14:41:53 +0800

    INIT

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,33 +1,21 @@
 <template>
 <div>
   <h1>Home</h1>
-  <computed></computed>
-  <!-- <shallow-ref></shallow-ref> -->
-  <!-- <custom-ref></custom-ref> -->
 </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-import unref from '../components/Unref.vue'
-import toref from '../components/Toref.vue'
-import customRef from '../components/CustomRef.vue'
-import shallowRef from '../components/ShallowRef.vue'
-import Computed from '../components/Computed.vue'
+
 export default {
     components: {
-        unref,
-        toref,
-        customRef,
-        shallowRef,
-        Computed
     },
     setup() {
         const count = ref(0);
         return {count}
     },
     mounted() {
-        console.log(this.$options.render)
+        
     }
 }
 </script>
